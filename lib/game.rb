24c8e4b18abdc07d052740be98e06563cdfb82e4
Zachary Lehmann <zlehmann@gmail.com>
2018-10-10 00:24:13 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -94,7 +94,7 @@ class Game
   end
 
   def turn
-    input = Players::Human.Player.move(@board)
+    input = Player.Players::Human.move(@board)
     if self.board.valid_move?(input) == false
       return "invalid"
     end
