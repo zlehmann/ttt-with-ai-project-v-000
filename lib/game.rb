aeb1fc6331eeedeefefe569a2e2587e86710cb55
Zachary Lehmann <zlehmann@gmail.com>
2018-10-10 00:38:26 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -99,7 +99,7 @@ class Game
     if self.board.valid_move?(input) == false
       player.move(@board)
     else
-      self.board.update(input, player.token)
+      self.board.update(input, self.current_player.token)
     end
   end
 
