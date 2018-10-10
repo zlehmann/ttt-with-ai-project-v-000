5851da73c4c2e528372916d495f58fea6c4cbf34
Zachary Lehmann <zlehmann@gmail.com>
2018-10-10 01:29:52 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,23 +1,21 @@
 module Players
   class Computer < Player
-    corners = [0,2,6,8]
-    edges = [1,3,5,77]
+    corners = [1,3,7,9]
+    edges = [2,4,6,8]
 
     def move(board)
       if board.taken?(5) == false
         return "5"
       else
         corners.each do |pos|
-          if board[pos] == " "
-            output = pos + 1
-            return "#{output.to_s}"
+          if board.taken?(pos) == false
+            return "#{pos.to_s}"
           end
         end
 
         edges.each do |pos|
-          if board[pos] == " "
-            output = pos + 1
-            return "#{output.to_s}"
+          if board.taken?[pos] == false
+            return "#{pos.to_s}"
           end
         end
       end
